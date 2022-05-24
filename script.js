9657f96eedc5a30451de2768f243d96c11d526f9
Dianna Plaza <dianna.s.plaza@gmail.com>
2022-05-23 23:31:58 -0400

    added base pseudo code to script file

---
+++ script.js
@@ -0,0 +1,17 @@
+// A Cocktail Recipe finder
+
+// create an app object (*haven't decided on a name yet)
+
+// Initialize preset data in the dedicated propertiesimage.png
+    // -apiUrl (we will create two variables for the url by ingredient & url by ID)
+    // -userQuery (for the selected liquor chosen)
+
+    // Create a method (getUserQuery) to update the variable (userQuery) based on user input
+
+    // Create a function to create the drink list that matches the user selected liquor choice. Create li elements under the ul to populate the page (containing drink name & photo). Append to the page.
+
+    // Add event listener ('click') to the user's choice of drink that they want the recipe for.
+
+    // Create function to display the chosen drink's ingredients & recipe 
+
+    // Include a reset button to start over (*event listener*)
