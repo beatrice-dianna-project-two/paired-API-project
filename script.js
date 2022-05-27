1a5c7d89ac12150c8df34037e72af92a002452bc
Dianna Plaza <dianna.s.plaza@gmail.com>
2022-05-27 01:31:11 -0400

    added id to button in html, attempted to add event listener to button, but couldn't. pushing to git before shutting down computer

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // PSEUDO CODE - A Cocktail Recipe finder
-    // Go get a set of images & cocktail names from TheCocktailDB that match the user's selection of a base liquor
-    // Put images on the page as li elements
+    // Go get a set of images & cocktail names from TheCocktailDB that match the user's liquor selection
+    // Display drink names & images on the page as li elements
 
 // our namespacing app
 const cocktailApp = {};
+cocktailApp.drinkListDiv = document.querySelector(".drinkList")
 
    // -API Url by ingredient
     const cocktailURLByIngredient = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=";
@@ -42,10 +43,16 @@ const cocktailApp = {};
             drinkOptions.append(drinkName, drinkImage);
 
             // append that HTML to the drinkList (blank ul on html)
-            document.querySelector(".drinkList").appendChild(drinkOptions);
-
+            cocktailApp.drinkListDiv.appendChild(drinkOptions);
         });
     }
+    // Connecting event listener to the button ('submit') on the form element
+
+
+    // Create function to display the chosen drink's ingredients & recipe 
+
+    // Include a reset button to start over (*event listener*)
+
 
     // Getting drinks from the API
     cocktailApp.getDrinks = function(){
@@ -53,23 +60,12 @@ const cocktailApp = {};
 
         // the function that will kick off our code
     cocktailApp.init = function (){
+        
         cocktailApp.getDrinks();
         
-
     }
 
-    // -userQuery (for the selected liquor chosen)
 
-
-    // Create a method (getUserQuery) to update the variable (userQuery) based on user input
-
-    // Create a function to create the drink list that matches the user selected liquor choice. Create li elements under the ul to populate the page (containing drink name & photo). Append to the page.
-
-    // Add event listener ('submit') to the form element
-
-    // Create function to display the chosen drink's ingredients & recipe 
-
-    // Include a reset button to start over (*event listener*)
 
 
     // call the init method to start running our app!
